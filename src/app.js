016ede8078abba6531e798dd4f5e421b236a50db
Pandian-Arjunan <pandianarjunan2307@gmail.com>
2024-12-03 21:46:12 +0530

    expolring get,post,put,pat,delete

--- src/app.js
+++ src/app.js
@@ -1,13 +1,31 @@
 const express = require("express");
 const app = express();
 
-app.use("/test", (req, res) => {
-  res.send("server is running on the port 3000");
-});
-app.use("/", (req, res) => {
-  res.send("main page");
+app.use("/user", (req, res) => {
+  res.send("User");
 });
 
-app.listen(3000, () => {
-  console.log("app is listnening");
+app.get("/user", (req, res) => {
+  res.send({ name: "pandain", age: "23" });
+});
+
+app.post("/user", (req, res) => {
+  res.send("data has been successfully posted");
+});
+app.delete('/user', (req,res)=> {
+  res.send('Data successfully deleted')
+})
+
+app.patch('/user', (req,res)=> {
+  res.send('Data part has been modified')
+})
+
+app.put('/user', (req,res)=> {
+  res.send("data has been updated")
+})
+
+
+
+app.listen(3001, () => {
+  console.log("server is running in port 3001");
 });
